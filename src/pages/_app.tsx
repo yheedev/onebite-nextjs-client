90e0aa66d56bb6fc4d8b218cfb61a1e87093453f
yheedev <yheedev@gmail.com>
2025-08-21 17:34:34 +0900

    11강: CSR navigating

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onClickBtn = () => {
+    router.push("/test");
+  };
+
   return (
     <>
-      <h1>헤더</h1>
+      <header>
+        <Link href={"/"}>main</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>book 1</Link>
+        <div>
+          <button onClick={onClickBtn}>/test 페이지로 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
   );
