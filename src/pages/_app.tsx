e3990aee70850946f7685e412b6bb2b329dfc784
yheedev <yheedev@gmail.com>
2025-08-21 17:06:52 +0900

    10강: 페이지 라우터 [id], [...id], [[...id], 404

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,10 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <h1>헤더</h1>
+      <Component {...pageProps} />
+    </>
+  );
 }
