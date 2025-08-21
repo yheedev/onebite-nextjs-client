36d87e85e279f36e5f5be2cfe4fd4b652a4aa55b
yheedev <yheedev@gmail.com>
2025-08-21 19:47:44 +0900

    14강: styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import style from "./index.module.css";
+
 export default function Home() {
   return (
     <>
-      <h1>인덱스</h1>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>h2 테스트</h2>
     </>
   );
 }
