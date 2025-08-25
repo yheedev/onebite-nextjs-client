d2bb652be0e7894b17af9cec7160a4b733619063
yheedev <yheedev@gmail.com>
2025-08-25 14:33:30 +0900

    15강: global layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,11 @@
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import "../styles/globals.css";
+import GlobalLayout from "../components/global-layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  const onClickBtn = () => {
-    router.push("/test");
-  };
-
-  useEffect(() => {
-    router.prefetch("/test");
-  }, []);
-
   return (
-    <>
-      <header>
-        <Link href={"/"}>main</Link>
-        &nbsp;
-        <Link href={"/search"}>search</Link>
-        &nbsp;
-        <Link href={"/book/1"}>book 1</Link>
-        <div>
-          <button onClick={onClickBtn}>/test 페이지로 이동</button>
-        </div>
-      </header>
+    <GlobalLayout>
       <Component {...pageProps} />
-    </>
+    </GlobalLayout>
   );
 }
