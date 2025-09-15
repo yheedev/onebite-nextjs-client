abbd51fd545088c153f10ba9a27d25e25933cce6
stillcorners <yheedev@gmail.com>
2025-09-15 17:17:41 +0900

    (wip)51강: server action

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -2,15 +2,20 @@ import Image from "next/image";
 import style from "./[id].module.css";
 import { notFound } from "next/navigation";
 
-export const dynamicParams = false;
+//export const dynamicParams = false;
 
 export function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
 
-export default async function Page({ params }: { params: Promise<{ id: string | string[] }> }) {
-  const { id } = await params;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`);
+async function BookDetail({
+  bookId,
+}: {
+  bookId: string;
+}) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`
+  );
   if (!res.ok) {
     if (res.status === 404) {
       notFound();
@@ -19,19 +24,72 @@ export default async function Page({ params }: { params: Promise<{ id: string | 
   }
 
   const book = await res.json();
-  const { title, subTitle, description, author, publisher, coverImgUrl } = book;
+  const {
+    title,
+    subTitle,
+    description,
+    author,
+    publisher,
+    coverImgUrl,
+  } = book;
 
   return (
-    <div className={style.container}>
+    <section>
       <div className={style.cover_img_container}>
-        <Image src={coverImgUrl} alt={title} fill priority sizes='(max-width: 768px) 100vw, 600px' style={{ objectFit: "cover" }} />
+        <Image
+          src={coverImgUrl}
+          alt={title}
+          fill
+          priority
+          sizes="(max-width: 768px) 100vw, 600px"
+          style={{ objectFit: "cover" }}
+        />
       </div>
       <div className={style.title}>{title}</div>
-      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.subTitle}>
+        {subTitle}
+      </div>
       <div className={style.author}>
         {author} | {publisher}
       </div>
-      <div className={style.description}>{description}</div>
+      <div className={style.description}>
+        {description}
+      </div>
+    </section>
+  );
+}
+
+function ReviewEditor() {
+  async function createReviewAction() {
+    "use server";
+  }
+
+  return (
+    <section>
+      <form action={createReviewAction}>
+        <input
+          name="content"
+          placeholder="리뷰를 작성해 주세요."
+        />
+        <input
+          name="author"
+          placeholder="리뷰 작성자"
+        />
+        <button type="submit">작성하기</button>
+      </form>
+    </section>
+  );
+}
+
+export default function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return (
+    <div className={style.container}>
+      <BookDetail bookId={params.id} />
+      <ReviewEditor />
     </div>
   );
 }
