6a649755801e053ca3336ef6cd52803260941cf8
yheedev <yheedev@gmail.com>
2025-09-25 00:15:58 +0900

    56강: 클라이언트 컴포넌트의 서버액션(리액트 훅 폼)

--- src/app/components/review-editor.tsx
+++ src/app/components/review-editor.tsx
@@ -1,15 +1,28 @@
+"use client";
+
 import style from "./review-editor.module.css";
 import { createReviewAction } from "@/actions/create-review-action";
+import { useActionState, useEffect } from "react";
 
 export default function ReviewEditor({ bookId }: { bookId: string }) {
+  const [state, formAction, isPending] = useActionState(createReviewAction, null);
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
   return (
     <section>
-      <form className={style.form_container} action={createReviewAction}>
+      <form className={style.form_container} action={formAction}>
         <input name='bookId' value={bookId} hidden readOnly />
-        <textarea required name='content' placeholder='리뷰를 작성해 주세요.' />
+        <textarea required disabled={isPending} name='content' placeholder='리뷰를 작성해 주세요.' />
         <div className={style.submit_container}>
-          <input required name='author' placeholder='리뷰 작성자' />
-          <button type='submit'>작성하기</button>
+          <input required disabled={isPending} name='author' placeholder='리뷰 작성자' />
+          <button disabled={isPending} type='submit'>
+            {isPending ? "저장 중..." : "작성하기"}
+          </button>
         </div>
       </form>
     </section>
